66f8e2fc1fdf5b7f3cadf618e7562c115bce823f
fantasyup <59591096+fantasyup@users.noreply.github.com>
2024-02-08 11:17:52 +0200

    add: uups upgradeable proxy impl

--- contracts/interfaces/IBSPair.sol
+++ contracts/interfaces/IBSPair.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.1;
 
 interface IBSPair {
-    
+    event UpdatedPriceOracle(address oldOracle, address newOracle);
+
+    function deposit(uint256 amount) external returns(uint);
+    function depositWithPermit() external returns(uint);
+    function borrow() external returns(uint);
+    function updatePriceOracle() external returns(address);
+
 }
