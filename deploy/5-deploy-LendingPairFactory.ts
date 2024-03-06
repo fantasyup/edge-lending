3e79d69a6342c19821fe1d34c34671330fa7e6e5
fantasyup <59591096+fantasyup@users.noreply.github.com>
2024-03-06 21:16:30 +0300

    remove: TODO.md

--- deploy/5-deploy-LendingPairFactory.ts
+++ deploy/5-deploy-LendingPairFactory.ts
@@ -3,14 +3,20 @@ import {DeployFunction} from 'hardhat-deploy/types';
 import { ContractId, EthereumAddress } from "../helpers/types"
 
 const deployLendingPairFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments: { deploy }, getNamedAccounts } = hre;
+    const { deployments: { deploy, get }, getNamedAccounts } = hre;
     const { deployer, blackSmithTeam } = await getNamedAccounts();
-  
+
+    const dataTypesLib = await get(ContractId.DataTypes)
+
     await deploy(ContractId.LendingPairFactory, {
       from: deployer,
       args: [blackSmithTeam],
       log: true,
-    }); 
+      libraries: {
+        [ContractId.DataTypes]: dataTypesLib.address
+      }
+    });
 }
+
 export default deployLendingPairFactory
 deployLendingPairFactory.tags = [`${ContractId.LendingPairFactory}`]
