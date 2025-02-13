9b95b0b333bca33b44ec8befde0cec9bb619ae1b
fantasyup <59591096+fantasyup@users.noreply.github.com>
2025-02-14 03:52:32 +1000

    wrote script for creating new lending pair

---
+++ deploy/16-create-NewLendingPair.ts
@@ -0,0 +1,41 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import { ContractId } from "../helpers/types"
+import { getLendingPairFactoryDeployment } from '../helpers/contracts';
+const deployLendingPairFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+
+  const lendingPairFactory = await getLendingPairFactoryDeployment()
+  if (!process.env.LendingPairName || !process.env.LendingPairSymbol || !process.env.PauseGuardian
+    || !process.env.CollateralAsset || !process.env.BorrowAsset || !process.env.InitialExchangeRateMantissa ||
+    !process.env.ReserveFactorMantissa || !process.env.CollateralFactor || !process.env.LiquidationFee || !process.env.InterestRateModel) {
+    console.log("Input Error")
+    return
+  }
+    const _lendingPairName = process.env.LendingPairName;
+    const _lendingPairSymbol = process.env.LendingPairSymbol;
+    const _pauseGuardian = process.env.PauseGuardian;
+    const _collateralAsset = process.env.CollateralAsset;
+    
+    const borrowAsset = process.env.BorrowAsset;
+    const initialExchangeRateMantissa = process.env.InitialExchangeRateMantissa;
+    const reserveFactorMantissa =process.env.ReserveFactorMantissa;
+    const collateralFactor=process.env.CollateralFactor;
+    const liquidationFee=process.env.LiquidationFee;
+    const interestRateModel = process.env.InterestRateModel;
+    
+    const _borrowVars = {borrowAsset,initialExchangeRateMantissa,reserveFactorMantissa,collateralFactor,liquidationFee,interestRateModel};
+
+    const tx = await lendingPairFactory.createLendingPairWithProxy(_lendingPairName,_lendingPairSymbol, _pauseGuardian, _collateralAsset,_borrowVars);
+    
+    console.log("New Lending Pair Created tx:",tx.hash)
+}
+
+export default deployLendingPairFactory
+deployLendingPairFactory.tags = [ContractId.LendingPairFactory]
+deployLendingPairFactory.dependencies = [
+  ContractId.LendingPair,
+  ContractId.DebtToken,
+  ContractId.CollateralWrapperToken,
+  ContractId.WrapperToken,
+  ContractId.RewardDistributorManager
+]
