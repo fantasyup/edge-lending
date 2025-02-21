a930b872785e4e5a706ef6e00d0608737e1bd6df
fantasyup <59591096+fantasyup@users.noreply.github.com>
2025-02-21 16:24:07 -0500

    rebase from feat/v1

--- deploy/16-deploy-LiquidationHelper.ts
+++ deploy/16-deploy-LiquidationHelper.ts
@@ -1,33 +1,33 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
-import { ContractId } from "../helpers/types";
+// import { HardhatRuntimeEnvironment } from "hardhat/types";
+// import { DeployFunction } from "hardhat-deploy/types";
+// import { ContractId } from "../helpers/types";
 
-import { ElementPTLiqudidationInfo } from "../helpers/constants";
+// import { ElementPTLiqudidationInfo } from "../helpers/constants";
 
-const deployLiquidationHelper: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {
-    deployments: { deploy, get },
-    getNamedAccounts,
-  } = hre;
-  const { deployer } = await getNamedAccounts();
+// const deployLiquidationHelper: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+//   const {
+//     deployments: { deploy, get },
+//     getNamedAccounts,
+//   } = hre;
+//   const { deployer } = await getNamedAccounts();
 
-  const {
-    BalancerV2Vault,
-    AaveV2LendingPoolAddressProvider,
-    ElementPTs,
-  } = ElementPTLiqudidationInfo;
+//   const {
+//     BalancerV2Vault,
+//     AaveV2LendingPoolAddressProvider,
+//     ElementPTs,
+//   } = ElementPTLiqudidationInfo;
 
-  const balancerPoolIds = ElementPTs.map((e) => e.balancerPTPoolId);
-  const edgePairs = ElementPTs.map((e) => e.edgePair);
-  const vault = await get(ContractId.Vault)
+//   const balancerPoolIds = ElementPTs.map((e) => e.balancerPTPoolId);
+//   const edgePairs = ElementPTs.map((e) => e.edgePair);
+//   const vault = await get(ContractId.Vault)
 
-  await deploy(ContractId.LiquidationHelper, {
-    from: deployer,
-    args: [AaveV2LendingPoolAddressProvider, BalancerV2Vault, vault.address, balancerPoolIds, edgePairs],
-    log: true,
-    deterministicDeployment: true,
-  });
-};
+//   await deploy(ContractId.LiquidationHelper, {
+//     from: deployer,
+//     args: [AaveV2LendingPoolAddressProvider, BalancerV2Vault, vault.address, balancerPoolIds, edgePairs],
+//     log: true,
+//     deterministicDeployment: true,
+//   });
+// };
 
-export default deployLiquidationHelper;
-deployLiquidationHelper.tags = [`${ContractId.LiquidationHelper}`];
+// export default deployLiquidationHelper;
+// deployLiquidationHelper.tags = [`${ContractId.LiquidationHelper}`];
